b03bc432a72245dd410c32ffa8e67aa87f5784ad
moganesan <developer.moganesan@gmail.com>
2024-07-30 20:37:04 +0530

    script migrated from web3js to ethersjs

--- src/universalRouter.ts
+++ src/universalRouter.ts
@@ -1,7 +1,6 @@
 import { SwapRouter, UniswapTrade } from "@uniswap/universal-router-sdk";
 import {
   TradeType,
-  Ether,
   Token,
   CurrencyAmount,
   Percent,
@@ -26,7 +25,7 @@ import {
 } from "@uniswap/router-sdk";
 import IUniswapV3Pool from "@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json";
 import IUniswapV3Factory from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
-import { parseEther, MaxUint256 } from "ethers";
+import * as ethers from "ethers";
 import { AllowanceTransfer } from "@uniswap/permit2-sdk";
 import wethAbi from "./wethAbi.json";
 import { config } from "../config";
@@ -36,16 +35,14 @@ import {
   ApiBaseUrl,
 } from "@fireblocks/fireblocks-web3-provider";
 import PERMIT_2_ABI from "./permit2Abi.json";
-import Web3, { Web3BaseProvider } from "web3";
+import Web3, { ProviderError } from "web3";
 import { MAX_UINT160 } from "@uniswap/smart-order-router";
 
 const SWAP_ROUTER_ADDRESS = config.UNISWAP_UNIVERSAL_ROUTER_ADDRESS;
 
 const WETH_ADDRESS = config.WETH_ADDRESS;
 
-const PERMIT2_ADDRESS = config.PERMIT2_ADDRESS;
-
-const ETHER = Ether.onChain(1);
+const PERMIT2_ADDRESS = config.UNISWAP_PERMIT2_ADDRESS;
 
 const MAX_UINT =
   "115792089237316195423570985008687907853269984665640564039457584007913129639935";
@@ -54,7 +51,7 @@ const WETH = new Token(ChainId.SEPOLIA, WETH_ADDRESS, 18);
 
 const makePermit = (
   tokenAddress,
-  amount = MaxUint256.toString(),
+  amount = ethers.constants.MaxUint256.toString(),
   nonce = "0"
 ) => {
   return {
@@ -77,66 +74,13 @@ async function generatePermitSignature(permit, signer, chainId) {
   );
   return await signer._signTypedData(domain, types, values);
 }
-
-// export async function getPool(
-//   web3: Web3,
-//   tokenA: Token,
-//   tokenB: Token,
-//   feeAmount: FeeAmount
-// ): Promise<Pool> {
-//   const [token0, token1] = tokenA.sortsBefore(tokenB)
-//     ? [tokenA, tokenB]
-//     : [tokenB, tokenA]; // does safety checks
-//   const poolAddress = Pool.getAddress(token0, token1, feeAmount);
-//   const contract = new web3.eth.Contract(IUniswapV3Pool.abi, poolAddress);
-//   let liquidity = await contract.methods.liquidity();
-//   let { sqrtPriceX96, tick } = await contract.methods.slot0();
-//   liquidity = JSBI.BigInt(liquidity.toString());
-//   sqrtPriceX96 = JSBI.BigInt(sqrtPriceX96.toString());
-
-//   return new Pool(
-//     token0,
-//     token1,
-//     feeAmount,
-//     sqrtPriceX96,
-//     liquidity,
-//     JSBI.toNumber(tick),
-//     [
-//       {
-//         index: nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount]),
-//         liquidityNet: liquidity,
-//         liquidityGross: liquidity,
-//       },
-//       {
-//         index: nearestUsableTick(TickMath.MAX_TICK, TICK_SPACINGS[feeAmount]),
-//         liquidityNet: JSBI.multiply(liquidity, JSBI.BigInt("-1")),
-//         liquidityGross: liquidity,
-//       },
-//     ]
-//   );
-// }
-
-interface LiquidityResult {
-  _hex: string;
-  _isBigNumber: boolean;
-}
-
-interface Slot0Result {
-  sqrtPriceX96: string;
-  tick: string;
-  observationIndex: string;
-  observationCardinality: string;
-  observationCardinalityNext: string;
-  feeProtocol: string;
-  unlocked: boolean;
-}
-
 export async function getPool(
-  web3: Web3,
+  provider: ethers.providers.Web3Provider,
   tokenA: Token,
   tokenB: Token,
   feeAmount: FeeAmount
 ): Promise<any> {
+  console.log("Get Pool Called");
   const [token0, token1] = tokenA.sortsBefore(tokenB)
     ? [tokenA, tokenB]
     : [tokenB, tokenA];
@@ -144,78 +88,68 @@ export async function getPool(
   let poolAddress;
 
   try {
-    const factoryContract = new web3.eth.Contract(
+    const factoryContract = new ethers.Contract(
+      config.UNISWAP_FACTORY_CONTRACT_ADDRESS,
       IUniswapV3Factory.abi,
-      config.UNISWAP_FACTORY_CONTRACT_ADDRESS
+      provider.getSigner()
     );
-    const getPoolAddress = await factoryContract.methods
-      .getPool(token0.address, token1.address, feeAmount)
-      .call();
+    const getPoolAddress = await factoryContract.getPool(
+      token0.address,
+      token1.address,
+      feeAmount
+    );
 
     console.log("Pool Address", getPoolAddress);
     poolAddress = getPoolAddress;
   } catch (err) {
     console.log("Error from gettingPool Address", err);
+    throw err;
   }
 
   // Check if the contract exists
-  const code = await web3.eth.getCode(poolAddress, "latest");
-  if (code === "0x") {
+  if (poolAddress === ethers.constants.AddressZero) {
     throw new Error(`No contract found at address ${poolAddress}`);
   }
 
   let poolContract;
   try {
-    const contract = new web3.eth.Contract(
+    poolContract = new ethers.Contract(
+      poolAddress,
       IUniswapV3Pool.abi as any,
-      poolAddress
+      provider.getSigner()
     );
-    poolContract = contract;
   } catch (err) {
     console.log("Error from initializing pool contract", err);
+    throw err;
   }
 
   try {
     const [liquidity, slot0] = await Promise.all([
-      poolContract.methods
-        .liquidity()
-        .call()
-        .catch((e) => {
-          console.error("Error calling liquidity():", e);
-          throw e;
-        }),
-      poolContract.methods
-        .slot0()
-        .call()
-        .catch((e) => {
-          console.error("Error calling slot0():", e);
-          throw e;
-        }),
+      poolContract.liquidity(),
+      poolContract.slot0(),
     ]);
 
-    const liquidityString =
-      typeof liquidity === "object" ? liquidity._hex : liquidity.toString();
-
-    const sqrtPriceX96String = slot0.sqrtPriceX96.toString();
-    const tickNumber = parseInt(slot0.tick, 10);
+    const liquidityBN = ethers.BigNumber.from(liquidity);
+    const sqrtPriceX96BN = ethers.BigNumber.from(slot0.sqrtPriceX96);
+    const tickNumber = slot0.tick;
 
     return new Pool(
       token0,
       token1,
       feeAmount,
-      sqrtPriceX96String as BigintIsh,
-      liquidityString as BigintIsh,
+      sqrtPriceX96BN.toString(),
+      liquidityBN.toString(),
       tickNumber,
       [
         {
           index: nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount]),
-          liquidityNet: liquidityString as BigintIsh,
-          liquidityGross: liquidityString as BigintIsh,
+          liquidityNet: liquidityBN.toString(),
+          liquidityGross: liquidityBN.toString(),
         },
         {
           index: nearestUsableTick(TickMath.MAX_TICK, TICK_SPACINGS[feeAmount]),
-          liquidityNet: `-${liquidityString}` as BigintIsh,
-          liquidityGross: liquidityString as BigintIsh,
+          liquidityNet: liquidityBN.mul(-1).toString(), // Convert to negative BigNumber
+          liquidityGross: liquidityBN.toString(),
         },
       ]
     );
@@ -269,21 +203,21 @@ function swapOptions(options, recipient) {
 }
 
 const setAllowance = async (
-  web3: Web3,
+  provider: ethers.providers.Web3Provider,
   tokenContract,
   owner,
   spender,
   amount
 ) => {
   try {
-    const contract = new web3.eth.Contract(wethAbi, tokenContract);
-    const currentAllowance = await contract.methods
-      .allowance(owner, spender)
-      .call();
+    const contract = new ethers.Contract(
+      tokenContract,
+      wethAbi,
+      provider.getSigner()
+    );
+    const currentAllowance = await contract.allowance(owner, spender);
     if (currentAllowance < amount) {
-      const tx = await contract.methods
-        .approve(spender, amount)
-        .send({ from: owner });
+      const tx = await contract.approve(spender, amount);
       console.log(tx);
     } else {
       console.log("Allowance Already Set");
@@ -293,71 +227,80 @@ const setAllowance = async (
   }
 };
 
-const getWeth = async (web3: Web3, amount: string) => {
-  const accounts = await web3.eth.getAccounts();
+const getWeth = async (
+  provider: ethers.providers.Web3Provider,
+  amount: string
+) => {
+  const signer = provider.getSigner();
+  const accounts = await signer.getAddress();
   try {
-    const contract = new web3.eth.Contract(wethAbi, WETH.address);
+    const contract = new ethers.Contract(WETH.address, wethAbi, signer);
 
-    console.log(
-      "Before Balance WETH:",
-      await contract.methods.balanceOf(accounts[0]).call()
-    );
-    await contract.methods
-      .deposit(amount)
-      .send({ from: accounts[0], value: amount });
+    console.log("Before Balance WETH:", await contract.balanceOf(accounts[0]));
+    await contract.deposit(amount);
 
-    console.log(
-      "After Balance WETH:",
-      await contract.methods.balanceOf(accounts[0]).call()
-    );
+    console.log("After Balance WETH:", await contract.balanceOf(accounts[0]));
   } catch (err) {
     console.log("Error from converting weth", err);
   }
 };
 
-export const swapTokens = async () => {
+export const swapTokens = async (
+  tokenAddress: string,
+  vaultId: string,
+  amountIn: string
+) => {
   const eip1193Provider = new FireblocksWeb3Provider({
     apiBaseUrl: ApiBaseUrl.Sandbox, // If using a sandbox workspace
     privateKey: config.FIREBLOCKS_SECRET_KEY,
     apiKey: config.FIREBLOCKS_API_KEY,
-    vaultAccountIds: ["50"],
+    vaultAccountIds: [vaultId],
     chainId: ChainId.SEPOLIA,
   });
 
-  const web3 = new Web3(eip1193Provider);
+  const provider = new ethers.providers.Web3Provider(eip1193Provider);
 
-  const address = await web3.eth.getAccounts();
+  const signer = provider.getSigner();
+  const address = await signer.getAddress();
 
-  web3.eth.defaultAccount = address[0];
+  console.log(tokenAddress);
 
-  const TOKEN_B = new Token(
-    ChainId.SEPOLIA,
-    "0x4f7A67464B5976d7547c860109e4432d50AfB38e",
-    18
+  const TOKEN_B = new Token(ChainId.SEPOLIA, tokenAddress, 18);
+  console.log(TOKEN_B);
+
+  const pool = await getPool(provider, WETH, TOKEN_B, FeeAmount.MEDIUM);
+
+  const inputEther = ethers.utils.parseEther(amountIn);
+
+  await setAllowance(
+    provider,
+    WETH.address,
+    address,
+    PERMIT2_ADDRESS,
+    MAX_UINT
   );
-
-  const pool = await getPool(web3, WETH, TOKEN_B, FeeAmount.MEDIUM);
-  const inputEther = parseEther("0.01");
-
-  await setAllowance(web3, WETH.address, address[0], PERMIT2_ADDRESS, MAX_UINT);
 
   //   await getWeth(web3, parseEther("0.09").toString());
 
-  const permit2 = new web3.eth.Contract(PERMIT_2_ABI, PERMIT2_ADDRESS);
+  const permit2 = new ethers.ethers.Contract(
+    PERMIT2_ADDRESS,
+    PERMIT_2_ABI,
+    signer
+  );
 
-  const checkPermit: any = await permit2.methods
-    .allowance(address[0], WETH_ADDRESS, SWAP_ROUTER_ADDRESS)
-    .call();
+  const checkPermit: any = await permit2.allowance(
+    address,
+    WETH_ADDRESS,
+    SWAP_ROUTER_ADDRESS
+  );
 
   if (Number(checkPermit.expiration) == 0) {
-    const txApproval = await permit2.methods
-      .approve(
-        WETH_ADDRESS,
-        SWAP_ROUTER_ADDRESS,
-        MAX_UINT160,
-        20_000_000_000_000
-      )
-      .send({ from: address[0] });
+    const txApproval = await permit2.approve(
+      WETH_ADDRESS,
+      SWAP_ROUTER_ADDRESS,
+      MAX_UINT160,
+      20_000_000_000_000
+    );
 
     console.log("Approval Tx", txApproval);
   }
@@ -368,35 +311,34 @@ export const swapTokens = async () => {
     TradeType.EXACT_INPUT
   );
 
-  const opts = swapOptions({}, address[0]);
+  const opts = swapOptions({}, address);
 
   const methodParameters = SwapRouter.swapCallParameters(
     new UniswapTrade(buildTrade([trade]), opts),
-    { sender: address[0] }
+    { sender: address }
   );
-  const gasPrice = await web3.eth.getGasPrice();
 
-  const estimatedGas = await web3.eth.estimateGas({
-    from: address[0],
+  const gasPrice = await provider.getGasPrice();
+
+  const estimatedGas = await provider.estimateGas({
+    from: address,
     to: SWAP_ROUTER_ADDRESS,
     value: inputEther.toString(),
     data: methodParameters.calldata,
   });
   console.log("Balance");
-  console.log(await web3.eth.getBalance(address[0]));
+  console.log(await provider.getBalance(address));
 
-  console.log(methodParameters.value);
-
-  const tx = await web3.eth.sendTransaction({
-    from: address[0],
+  const tx = await signer.sendTransaction({
+    from: address,
     to: SWAP_ROUTER_ADDRESS,
-    value: inputEther.toString(),
+    value: methodParameters.value,
     data: methodParameters.calldata,
     gasPrice: gasPrice,
-    gas: estimatedGas,
+    gasLimit: estimatedGas,
   });
-
-  console.log(tx);
+  tx.wait();
+  return tx;
 };
 
-swapTokens();
+swapTokens("0x4f7A67464B5976d7547c860109e4432d50AfB38e", "50", "0.01");
